bc30f4f76bfa8f4dff0866d2a4f6e00a5dfaf4fe
weeger <romain.weeger@gmail.com>
2024-10-27 13:52:17 +0100

    Use external config

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wexample-config',
+    version=open('version.txt').read(),
+    author='weeger',
+    author_email='contact@wexample.com',
+    description='Helpers to manage dict types configurations.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/wexample/python-config',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.6',
+)
