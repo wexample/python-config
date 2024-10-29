c63d6d94b46db7311fc6d79a2c330b7d7f087f86
weeger <romain.weeger@gmail.com>
2024-10-29 13:06:08 +0100

    Various

--- wexample_config/const/types.py
+++ wexample_config/const/types.py
@@ -4,4 +4,4 @@ from wexample_helpers.const.types import StringKeysDict, BasicValue
 # We may use the future __extra_items__ flag in python 3.13.
 # We can still be confident to the internal config check process.
 DictConfig = StringKeysDict
-ConfigValue = BasicValue
+DictConfigValue = BasicValue
