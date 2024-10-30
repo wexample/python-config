e349d0884967e563a14f077ba1d77a994e8d5abf
weeger <romain.weeger@gmail.com>
2024-10-30 09:02:28 +0100

    Config manager

--- wexample_config/src/demo_config_class.py
+++ wexample_config/src/demo_config_class.py
@@ -1,14 +1,5 @@
-from typing import List, Type
-from typing_extensions import TYPE_CHECKING
-from wexample_config.classes.config_manager import ConfigManager
-from wexample_config.options_provider.demo_options_provider import DemoOptionsProvider
-
-if TYPE_CHECKING:
-    from wexample_config.options_provider.abstract_options_provider import AbstractOptionsProvider
+from pydantic import BaseModel
 
 
-class DemoConfigClass(ConfigManager):
-    def get_options_providers(self) -> List[Type["AbstractOptionsProvider"]]:
-        return [
-            DemoOptionsProvider,
-        ]
+class DemoConfigClass(BaseModel):
+    pass
