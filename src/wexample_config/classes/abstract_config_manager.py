68976df09bf175aa259e07bd082f1301f79fd412
weeger <romain.weeger@gmail.com>
2025-10-27 19:11:41 +0100

    Rect

--- src/wexample_config/classes/abstract_config_manager.py
+++ src/wexample_config/classes/abstract_config_manager.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+from wexample_helpers.decorator.base_class import base_class
+
 from wexample_config.config_option.abstract_nested_config_option import (
     AbstractNestedConfigOption,
 )
-from wexample_helpers.decorator.base_class import base_class
 
 
 @base_class
